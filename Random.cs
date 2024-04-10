8df0e9e6d0fea430e26fd77377fc75de0053f648
Debbie Yuen <deborahyuen2016@berkeley.edu>
2024-04-10 07:54:28 -0700

    lecture 14.2 lab

---
+++ Random.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+
+public class Random : MonoBehaviour
+{
+    void RandomNumberClassic()
+    {
+
+    }
+
+    void RandomNumberUnityMathematics()
+    {
+
+    }
+}
